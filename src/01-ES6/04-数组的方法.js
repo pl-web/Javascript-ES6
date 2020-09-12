417dfc9a16b91600e1a73ac42a054a905598b512
it-reader <hnature@126.com>
2020-09-12 19:46:41 +0800

    map-and-filter-api

--- src/01-ES6/04-数组的方法.js
+++ src/01-ES6/04-数组的方法.js
@@ -1,6 +1,23 @@
 let arr = [1, 2, 3, 4, 5]
 
+
 // forEach
-arr.forEach(function(item, index, _arr) {
-  console.log(item, index, _arr)
+// forEach not support break and continue
+// arr.forEach(function(item, index, _arr) {
+//   console.log(item, index, _arr)
+// })
+
+
+// map
+let result = arr.map(function(value) {
+  return ++value
 })
+console.log(result)
+console.log(arr)
+
+
+// filter
+let res = arr.filter(function(value) {
+  return value >= 2
+})
+console.log(res)
