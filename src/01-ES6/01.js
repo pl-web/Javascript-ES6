897893d0f41b378400ed1df889a5fa3658b824cb
it-reader <hnature@126.com>
2020-09-12 15:18:06 +0800

    var-let-的不同

--- src/01-ES6/01.js
+++ src/01-ES6/01.js
@@ -1,10 +1,24 @@
-var a = 6
-console.log(a) // 6
+// var a = 6
+// console.log(a) // 6
 // delete 只能删除对象的属性
-delete a
-console.log(a) // 6
+// delete a
+// console.log(a) // 6
 
-b = 7
-console.log(b) // 7
-delete b
-console.log(b) // undefined
+// b = 7
+// console.log(b) // 7
+// delete b
+// console.log(b) // undefined
+
+// let c = 1
+// console.log(c) // 1
+// console.log(window.c) // undefined
+
+// let d = 2 
+// let d = 3 
+// console.log(d) // Identifier 'd' has already been declared
+
+// console.log("e:", e) // undefined
+// var e = 5 
+
+// console.log("f", f) // Cannot access 'f' before initialization
+// let f = 11
