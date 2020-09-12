3bdd1b0c55607f8f49e2df271f3c0c60b1f2e955
it-reader <hnature@126.com>
2020-09-12 19:59:16 +0800

    reduce累加器

--- src/01-ES6/04-数组的方法.js
+++ src/01-ES6/04-数组的方法.js
@@ -35,3 +35,13 @@ let res_every = arr.every(function(value) {
   return value == 2
 })
 console.log("every: ", res_every)  // false
+
+
+// reduce: 累加器
+// first params: function()
+// second params: initial value ：做加法之前的初始值
+let res_reduce = arr.reduce(function(prev, curr, index, arr_) {
+  console.log(prev, curr, index, arr_)
+  return prev + curr
+}, 0)
+console.log("reduce: ", res_reduce)
