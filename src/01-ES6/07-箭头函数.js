39674fb181e25386905e7a6ba8a08d67015eef7d
it-reader <hnature@126.com>
2020-09-13 07:54:46 +0800

    箭头函数

---
+++ src/01-ES6/07-箭头函数.js
@@ -0,0 +1,12 @@
+// () => { ... }
+function foo(x, y) {
+  return x + y
+}
+let add = function (x, y) {
+  return x + y
+}
+// let sum = (x, y) => {
+//   return x + y
+// }
+let sum = (x, y) => x + y
+console.log(sum(1, 2))
