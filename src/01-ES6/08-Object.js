c779b89a5a03b35ccb409e72877f24d144a33c21
it-reader <hnature@126.com>
2020-09-13 14:18:27 +0800

    遍历对象的方式

--- src/01-ES6/08-Object.js
+++ src/01-ES6/08-Object.js
@@ -38,6 +38,34 @@ console.log(y)
 let arr = [1, 2, 3]
 console.log(3 in arr) // false
 let obj_a = {
-  a: 1
+  a: 1,
+  b: 2,
+  c: "112"
 }
 console.log("a" in obj_a) // true
+
+
+// 对象的循环 -01
+for(let key in obj_a) {
+  console.log("对象的循环-01：", obj_a[key])
+}
+
+// 对象的循环 -02
+Object.keys(obj_a).forEach(key => {
+  console.log("对象的循环-02：", obj_a[key])
+})
+
+// 对象的循环 -03
+Object.getOwnPropertyNames(obj_a).forEach(key => {
+  console.log("对象的循环-03：", obj_a[key])
+})
+
+// 对象的循环 -04
+Reflect.ownKeys(obj_a).forEach(key => {
+  console.log("对象的循环-04：", obj_a[key])
+})
+
+// x
+// obj_a.forEach(item => {
+//   console.log(obj_a.item)
+// })
