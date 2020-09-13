1375ab42420b7ed1b06b71cba229fef47209b1e2
it-reader <hnature@126.com>
2020-09-13 22:08:36 +0800

    static静态属性和方法

--- src/02-ES6/03-ES6继承.js
+++ src/02-ES6/03-ES6继承.js
@@ -1,21 +1,43 @@
 // ES6 class
 class Person{
-
   constructor(name, age) {
     this.name = name
     this.age = age
+    // _sex_
+    this._gender_ = -1
+  }
+
+  // 只读属性gender
+  get gender() {
+    return this._gender_
+  }
+
+  // 设置属性gender
+  set gender(gender) {
+    this._gender_ = gender
   }
 
   // methods
   sayHello() {
     console.log("Hello World!")
   }
+
+  // 静态方法
+  static sayHi() {
+    return "Hi"
+  }
 }
 
 let p = new Person("SeedOrbit", 24)
 
+console.log("33: ", Person.sayHi())
+
 console.log("Person: ", p)
 
+// 测试set&get
+console.log("gender: " + p.gender)
+p.gender = "female"
+console.log("gender: " + p.gender)
 
 // extends 
 class Friend extends Person {
