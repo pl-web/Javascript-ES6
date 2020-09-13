c8ebc366877d3c0f08cca8f53277da650ec0cfed
it-reader <hnature@126.com>
2020-09-13 16:39:57 +0800

    静态属性与静态方法

--- src/02-ES6/01-面向过程&面向对象.js
+++ src/02-ES6/01-面向过程&面向对象.js
@@ -2,12 +2,24 @@
 // 类class 对象object
 // 类是对象的模板，定义了同一组对象共有的属性和方法
 function Person(name, age) {
+  // 实例属性
   this.name = name,
   this.age =  age
 }
+
+// 静态属性
+Person.number = 13
+
+// 静态方法
+Person.addNumber = function() {
+  Person.number++
+}
 let p1 = new Person("SeedOrbit", 24)
 console.log(p1)
+
 // p1.sayHello() is not a function
+
+// 实例方法
 Person.prototype.sayHello = function () {
   console.log("hello, everyone")
 }
