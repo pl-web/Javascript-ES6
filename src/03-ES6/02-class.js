342b6d75ba3669202a89bbd551cb7aa268fbfa56
it-reader <hnature@126.com>
2020-09-26 23:50:24 +0800

    class

---
+++ src/03-ES6/02-class.js
@@ -0,0 +1,16 @@
+class User{
+
+  constructor(name, password) {
+    this.name = name;
+    this.password = password;
+  }
+
+  showName() {
+    console.log(this.name);
+  }
+
+  showPassword() {
+    console.log(this.password);
+  }
+
+}
