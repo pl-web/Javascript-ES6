d91b40ddc7168a0bbff0877bb046eaa1436f6bc1
it-reader <hnature@126.com>
2020-09-26 23:59:13 +0800

    class-extends

--- src/03-ES6/02-class.js
+++ src/03-ES6/02-class.js
@@ -1,16 +1,24 @@
 class User{
-
   constructor(name, password) {
     this.name = name;
     this.password = password;
   }
-
   showName() {
     console.log(this.name);
   }
-
   showPassword() {
     console.log(this.password);
   }
 
 }
+class VipUser extends User {
+  constructor(name, password, level) {
+    super(name, password);
+    this.level = level;
+  }
+  showLevel() {
+    console.log(this.level);
+  }
+}
+const vip_u01 = new VipUser("pp", "123456", "2");
+console.log(vip_u01);
