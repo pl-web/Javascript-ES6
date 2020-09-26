e53d480ce02eee5fa9906790d2ee29562de4fabd
it-reader <hnature@126.com>
2020-09-26 23:12:41 +0800

    ES6-Learning

--- src/02-ES6/05-set.js
+++ src/02-ES6/05-set.js
@@ -25,3 +25,20 @@ for(let item of s.keys()) {
 for(let item of s.values()) {
   console.log(item)
 }
+
+
+// 数组的去重
+let arr = [1, 2, 3, 4, 5]
+let arr_ = [1, 2, 3, 4, 5, 6, 7]
+let s_ = new Set([...arr, ...arr_])
+console.log(s_)
+// 将s_转换为数组
+console.log([...s_])
+console.log(Array.from(s_))
+
+
+// 求交集
+let _arr = new Set([1,1,22,22])
+let _arr_ = new Set([11,22,33])
+let result = new Set([1,1,22,22].filter(item => _arr_.has(item)))
+console.log(Array.from(result))
---
+++ src/03-ES6/01.js
@@ -0,0 +1,10 @@
+// uicode 编码
+console.log("\u0061");
+
+// 可以对字符串进行遍历
+// for ... of
+for(let str of '0200') {
+  console.log(str)
+}
+
+// 模板字符串
